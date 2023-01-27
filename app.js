d601acb1eefdd2d94c9d2ddaeb5db05260cd743f
CarterSnich <jmestreraclosa@gmail.com>
2023-01-27 21:56:13 +0800

    removed index router

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
 
-let indexRouter = require("./routes/index");
 let apiRouter = require("./routes/api");
 
 let app = express();
@@ -14,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/api", apiRouter);
 
 module.exports = app;
